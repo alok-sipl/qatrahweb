96b087e4ce23d5723e45a768a5f5ab227f28cd5e
durgesh <durgesh.ahirwar@systematixindia.com>
2018-04-06 18:58:55 +0530

    worked on auth module

--- src/components/Auth/profile.js
+++ src/components/Auth/profile.js
@@ -153,16 +153,9 @@ class Profile extends Component {
   @Returns : *
   */
   removeInvalidChars(text) {
-    let newText = '';
-    let numbers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ';
 
-    for (var i = 0; i < text.length; i++) {
-      if ( numbers.indexOf(text[i]) > -1 ) {
-        newText = newText + text[i];
-      }
-    }
 
-    return newText;
+    return text;
   }
 
   /*
@@ -171,8 +164,7 @@ class Profile extends Component {
   @Returns : *
   */
   removeInvalidCharsAddress(text) {
-    let regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g
-    return text.replace(regex, '');
+    return text;
   }
   /*
   @Method : onChangeAddress
@@ -203,9 +195,11 @@ class Profile extends Component {
   {
     if(this.state.isNameEditActive)
     {
-      return(
+
+
+        return(
         <div>
-        <Input onChange={(event)=>{this.onChangeName(event.target.value)}} value={this.props.name} placeholder="Name"  placeholderTextColor="#fff"   style={styles.editProfileName.button} />
+        <input type="text" onChange={(event)=>{this.onChangeName(event.target.value)}} value={this.props.name} placeholder="Name"  placeholderTextColor="#fff"   style={styles.editProfileName.button} />
         <a  onClick={() => {this.updateName()}}><i className="fa fa-pencil" aria-hidden="true"></i></a>
         <a onClick={() => {this.setState({isNameEditActive:false})
         this.props.getUserDetails();
@@ -213,12 +207,14 @@ class Profile extends Component {
 
         </div>
 
-      );
+      )
     }
     else
     {
+
       return(
         <div>
+          <div>hello Anchal</div>
         <div> {this.props.name}  </div>
         <a  onClick={() => {this.setState({isNameEditActive:true})}}><i className="fa fa-pencil" aria-hidden="true"></i></a>
 
